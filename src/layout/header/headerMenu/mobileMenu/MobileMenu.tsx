81cb69b0642695e9c9c1ab35112693cff8f09dc3
Serg Nakovnick <w8smile@gmail.com>
2023-11-28 13:17:37 +0300

    Refactoring

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {S} from "./../HeaderMenu_Styles";
 import {Menu} from "../menu/Menu";
 
@@ -6,17 +6,16 @@ import {Menu} from "../menu/Menu";
 
 
 export const MobileMenu: React.FC <{menuItems: Array<string>}> = (props: {menuItems: Array<string>}) => {
+    const [menuIsOpen, setmenuIsOpen] = useState (false)
+    const onBurgerBtnClick = () => {setmenuIsOpen(!menuIsOpen)}
     return (
         <S.MobileMenu>
-            <S.BurgerButton is0pen={false}>
+            <S.BurgerButton is0pen={menuIsOpen} onClick={onBurgerBtnClick}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup is0pen={false}>
+            <S.MobileMenuPopup is0pen={menuIsOpen} onClick = { ()=>{setmenuIsOpen(false)} }>
                 <Menu menuItems={props.menuItems}/>
             </S.MobileMenuPopup>
         </S.MobileMenu>
     );
 };
-
-
-
